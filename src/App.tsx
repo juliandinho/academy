abc8d5882003359f0bca52106346c139e9394d47
juliandinho <julien0312@gmail.com>
2026-07-23 15:32:36 +0200

    changement de label

--- src/App.tsx
+++ src/App.tsx
@@ -127,7 +127,7 @@ const App: React.FC = () => {
                 className={styles.button}
                 onClick={() => window.open('https://aka.ms/codeapps', '_blank')}
               >
-                Get Started
+                Get Started MY BOY
               </Button>
             </div>
           </Card>
